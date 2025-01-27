36f1ce57a538688ff40506ee74f055fe632393b8
EloyPardo <pardogurreaeloy@gmail.com>
2025-01-27 12:01:18 +0100

    Creación Repo TradeQuest

---
+++ src/main/java/org/example/tradequestapp/TradeQuestAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.tradequestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeQuestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeQuestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/tradequestapp/TradeQuestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tradequestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeQuestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
