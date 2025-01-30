b6ba23aa0d5b712d1e5711f34cdcdf16f00a8ae2
EloyPardo <pardogurreaeloy@gmail.com>
2025-01-30 09:30:52 +0100

    Asset Entity Created

--- src/main/java/org/example/tradequestapp/entities/Asset.java
+++ src/main/java/org/example/tradequestapp/entities/Asset.java
@@ -1,5 +1,46 @@
 package org.example.tradequestapp.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Entity
+@Table(name = "Asset")
 public class Asset {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "asset_id", nullable = false, unique = true)
+    private Long asset_id;
+
+    @Column(name = "current_value")
+    private float current_value;
+
+    @Column(name = "opening_value")
+    private float opening_value;
+
+    @Column(name = "lowest_value")
+    private float lowest_value;
+
+    @Column(name = "highest_value")
+    private float highest_value;
+
+    @Column(name = "volume")
+    private float volume;
+
+    //Relaciones
+
+
+    public Asset(float opening_value, float lowest_value, float highest_value, float volume) {
+        this.current_value = opening_value; //Porque no podemos coger el valor in real time
+        this.opening_value = opening_value;
+        this.lowest_value = lowest_value;
+        this.highest_value = highest_value;
+        this.volume = volume;
+    }
 }
