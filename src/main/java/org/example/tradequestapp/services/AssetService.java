1090f2e43798aa872517848299f51f4b0eefa43a
Eloypg <pardogurreaeloy@gmail.com>
2025-02-09 15:51:15 +0100

    AssetService Basic Functions

--- src/main/java/org/example/tradequestapp/services/AssetService.java
+++ src/main/java/org/example/tradequestapp/services/AssetService.java
@@ -6,9 +6,37 @@ import org.example.tradequestapp.model.CompanyData;
 import org.example.tradequestapp.model.StockData;
 import org.example.tradequestapp.respositories.AssetRepository;
 import org.example.tradequestapp.respositories.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
 public class AssetService {
+
+    private final AssetRepository assetRepository;
+
+    @Autowired
+    public AssetService(AssetRepository assetRepository) {
+        this.assetRepository = assetRepository;
+    }
+
+    public Asset saveAsset(Asset asset){
+        return assetRepository.save(asset);
+    }
+
+    public List<Asset> getAllAssets(){
+        return assetRepository.findAll();
+    }
+
+    public Asset getAssetByCompanySymbol(String symbol){
+        return assetRepository.findByCompany_symbol(symbol);
+    }
+
+    public void deleteAsset(String symbol){
+        assetRepository.delete(assetRepository.findByCompany_symbol(symbol));
+    }
+
 }
