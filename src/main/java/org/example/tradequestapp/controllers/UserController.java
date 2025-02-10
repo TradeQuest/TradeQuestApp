c5dd558087a2156b5feb8162da7eec1375ff7860
Eloypg <pardogurreaeloy@gmail.com>
2025-02-10 19:40:26 +0100

    User Controller, Repository, Service --> Created

---
+++ src/main/java/org/example/tradequestapp/controllers/UserController.java
@@ -0,0 +1,42 @@
+package org.example.tradequestapp.controllers;
+
+import org.example.tradequestapp.entities.User;
+import org.example.tradequestapp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/userApi")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){this.userService = userService;}
+
+    @PostMapping("/user")
+    public ResponseEntity<User> saveUser(@RequestBody User user){
+        User newUser = userService.saveUser(user);
+        return ResponseEntity.ok(newUser);
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(){return userService.getAllUsers();}
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        Optional<User> user = userService.getUserById(id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return ResponseEntity.ok("User deleted successfully");
+    }
+
+}
---
+++ src/main/java/org/example/tradequestapp/respositories/UserRepository.java
@@ -0,0 +1,12 @@
+package org.example.tradequestapp.respositories;
+
+import org.example.tradequestapp.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    void delete(Optional<User> byId);
+}
