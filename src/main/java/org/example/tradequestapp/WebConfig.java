addaf21f5e6b15f2f7da6b7cb7346f1cc580fd23
jorge <jorgegarciabono@gmail.com>
2025-02-12 13:41:29 +0100

    Problema de visualización de estilos fixed

--- src/main/java/org/example/tradequestapp/WebConfig.java
+++ src/main/java/org/example/tradequestapp/WebConfig.java
@@ -3,7 +3,6 @@ package org.example.tradequestapp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,8 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry){
-        resourceHandlerRegistry.addResourceHandler("/css/**").addResourceLocations("classpath:/resources/static/css/");
-        resourceHandlerRegistry.addResourceHandler("/js/**").addResourceLocations("classpath:/resources/static/js/");
+    public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry) {
+        resourceHandlerRegistry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        resourceHandlerRegistry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        resourceHandlerRegistry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
     }
+
 }
