4261b86c5b637b1edd4b00b9ff8cc325a4fa7a70
EloyPardo <pardogurreaeloy@gmail.com>
2025-02-13 08:27:08 +0100

    FAQ Controller Add

--- src/main/java/org/example/tradequestapp/controllers/IndexController.java
+++ src/main/java/org/example/tradequestapp/controllers/IndexController.java
@@ -34,6 +34,9 @@ public class IndexController {
     @GetMapping("/wallet")
     public String wallet(){return "Wallet";}
 
+    @GetMapping("/faq")
+    public String faq(){return "FAQ";}
+
     @GetMapping("/policies")
     public String policies(){return "Policies";}
 
